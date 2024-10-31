f0189925dd596934cd8c503fec004cadfed0ff13
Edilberto Mario Rodríguez Fontalvo <edimarod@outlook.es>
2024-10-31 15:29:15 +0000

    feat(UH2000): api call created

---
+++ src/services/api.ts
@@ -0,0 +1,20 @@
+export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+
+export const fetcher = async (endpoint: string, options: RequestInit = {}) => {
+    const isFormData = options.body instanceof FormData;
+
+    const response = await fetch(`${BASE_URL}${endpoint}`, {
+        ...options,
+        headers: {
+        ...(isFormData ? {} : { "Content-Type": "application/json" }),
+        ...options.headers,
+        },
+    });
+
+    if (!response.ok) {
+        const errorMessage = await response.text();
+        throw new Error(errorMessage || "An error occurred");
+    }
+
+    return response.json();
+};
